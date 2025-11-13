76bf63c2b6dd863b88b75b8379d24be1334318fb
Striven100 <tobimislej@gmail.com>
2025-11-13 19:21:57 -0300

    API works!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ ok: true, message: "RPG API up" });
+  res.json({ ok: true, message: "API works!" });
 });
 
 export default router;
